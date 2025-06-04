38d32e7fe28bee5a891b37c28cc5e5c69e5ef6c9
EduGollner0609 <dudugollner@gmail.com>
2025-06-04 20:41:03 -0300

    Tratando exceção personalizada CpfExistsException.

--- backend/src/main/java/com/eduardo/user_cep_manager/controllers/handlers/ControllerExceptionHandler.java
+++ backend/src/main/java/com/eduardo/user_cep_manager/controllers/handlers/ControllerExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.eduardo.user_cep_manager.dtos.errors.CustomErrorDTO;
 import com.eduardo.user_cep_manager.services.ResourceNotFoundException;
+import com.eduardo.user_cep_manager.services.exceptions.CpfExistsException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -18,6 +19,13 @@ public class ControllerExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<CustomErrorDTO> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		CustomErrorDTO err = new CustomErrorDTO(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(CpfExistsException.class)
+	public ResponseEntity<CustomErrorDTO> cpfExistsException(CpfExistsException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		CustomErrorDTO err = new CustomErrorDTO(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
