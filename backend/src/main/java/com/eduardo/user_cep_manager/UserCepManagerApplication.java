23deb9fc866557a357fb43bb73aadf4084a33dba
EduGollner0609 <dudugollner@gmail.com>
2025-06-04 18:21:20 -0300

    Criando projeto backend.

---
+++ backend/src/main/java/com/eduardo/user_cep_manager/UserCepManagerApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.user_cep_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCepManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCepManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eduardo/user_cep_manager/UserCepManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.user_cep_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCepManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
