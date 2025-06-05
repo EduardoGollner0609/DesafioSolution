b83679548fd74851a6a80280282d4282eece0cbf
EduGollner0609 <dudugollner@gmail.com>
2025-06-05 04:27:05 -0300

    Estilizando HomePage.

--- frontend/src/routes/Page/HomePage/index.tsx
+++ frontend/src/routes/Page/HomePage/index.tsx
@@ -1,5 +1,21 @@
+import './styles.css';
+
 export default function HomePage() {
     return (
-        <h1>Olá</h1>
+        <main>
+            <section id="home-page-section" className="container">
+                <div className="card-welcome">
+                    <h3>Seja bem vindo</h3>
+                    <div className="card-welcome-text">
+                        <p>A Address Manager é uma aplicação web desenvolvida durante um processo seletivo. </p>
+                        <p> O desafio proposto foi criar uma aplicação que pudesse cadastrar usuários com seus endereços,
+                            buscados pelo cep. Venha dar uma olhada</p>
+                    </div>
+                    <div className="card-welcome-link">
+                        <a href="">Ver</a>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 }
