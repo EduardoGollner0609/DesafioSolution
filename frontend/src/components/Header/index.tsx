37ce1e25cad623dcc6ea9b3106ad9d2ce9c1e6e9
EduGollner0609 <dudugollner@gmail.com>
2025-06-05 03:38:48 -0300

    Estilizando Header.

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,8 +1,17 @@
+import './styles.css';
+import githubIcon from '../../assets/github-icon.svg';
+
 export default function Header() {
     return (
         <header>
-            <nav>
-                <h2>Solution</h2>
+            <nav className="container">
+                <h1>Address Manager</h1>
+                <div className="github-code-space">
+                    <a href="https://github.com/EduardoGollner0609/DesafioSolution">
+                        <img src={githubIcon} alt="Github Icon" />
+                        <p>Ver código fonte</p>
+                    </a>
+                </div>
             </nav>
         </header>
     );
