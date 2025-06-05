4b39bda573cc95a0ef3ac5eaa3e265f9ae7f3793
EduGollner0609 <dudugollner@gmail.com>
2025-06-05 03:22:54 -0300

    Preparando rotas com react router dom.

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <h2>Solution</h2>
+            </nav>
+        </header>
+    );
+}
---
+++ frontend/src/routes/Page/AddressListPage/index.tsx
@@ -0,0 +1,5 @@
+export default function AddressListPage() {
+    return (
+        <h1>Olá mundo</h1>
+    );
+}
