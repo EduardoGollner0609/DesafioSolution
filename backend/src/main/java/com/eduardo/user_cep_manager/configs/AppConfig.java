5bf4c5eaa5095d44eb335968d8090aae29d9f9fc
EduGollner0609 <dudugollner@gmail.com>
2025-06-07 13:21:43 -0300

    Configurando a permissão do cors separado no backend.

--- backend/src/main/java/com/eduardo/user_cep_manager/configs/AppConfig.java
+++ backend/src/main/java/com/eduardo/user_cep_manager/configs/AppConfig.java
@@ -1,26 +1,12 @@
 package com.eduardo.user_cep_manager.configs;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
-
-	@Value("${cors.origins}")
-	private String corsOrigins;
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		String[] origins = corsOrigins.split(",");
-
-		registry.addMapping("/**").allowedOrigins(origins).allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-				.allowedHeaders("*").allowCredentials(true);
-
-	}
 
 	@Bean
 	public RestTemplate restTemplate() {
